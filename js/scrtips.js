f9696752d2e1578844fce8f0b3fa66bc240713d9
tlpackus <tlpackus@gmail.com>
2021-02-08 18:48:06 -0800

    initial project set up with html css js and img and adding some js

---
+++ js/scrtips.js
@@ -0,0 +1,10 @@
+$(document).ready(function) {
+  const vowels = ["a","e","i","o","u"];
+  const inputString = []
+
+
+  function pigLatinTranslate(textToTranslate) {
+    inputString.push(textToTranslate);
+  }
+  pigLatinTranslate(pig latin);
+}
